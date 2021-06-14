b3eedab0484493b4ba11099a1188779a3cfb3bc7
ahmed elsayed hindi <ahmedelsyd5@gmail.com>
2021-06-15 01:29:14 +0200

    firstdata

---
+++ 01_brown_noise/brown_noise.py
@@ -0,0 +1,43 @@
+import sounddevice as sd
+from scipy.io import wavfile
+import numpy as np
+from typing import List, Tuple
+import sys
+
+
+def clean_data(data):
+    if data.shape[1] > 1:
+        data = data[:, 0]
+    return data[data > 0]
+
+
+def calc_time(data: np.array, samplerate: int) -> int:
+    """returns time of the audio file in seconds"""
+    return len(data) // samplerate
+
+
+def read_wav(file: str) -> Tuple[int, np.array]:
+    "returns samplerate, data"
+    return wavfile.read(file)
+
+
+def create_wav_with_time(data: np.array, sample_time: float, play_time: int) -> np.array:
+    multiples = (play_time) // sample_time
+    return np.concatenate([data] * multiples)
+
+
+def play(data: np.array, samplerate: int) -> None:
+    sd.play(data, samplerate=samplerate, mapping=None, blocking=True)
+
+
+def main():
+    time = 2
+    samplerate, data = read_wav("brown.wav")
+    data = clean_data(data)
+    sample_time = calc_time(data, samplerate)
+    data = create_wav_with_time(data, sample_time, play_time=time)
+    play(data, samplerate)
+    sys.exit()
+
+
+main()
